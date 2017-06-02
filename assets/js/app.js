01d72d4a7db30cb8fb623745c5542031fd6e0d32
Ariana Cabana <acabana.0904@gmail.com>
2017-06-02 18:04:26 -0500

    maquetacion

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+$( () => { //function() {}
+  $(".popup-link").on('click',(e) => {
+    e.preventDefault();
+    $('.brand-popup').show();
+    //$('.brand-popup').removeClass('hide');
+    //$('.brand-popup').addClass('show');
+
+  });
