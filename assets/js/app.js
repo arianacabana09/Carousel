218ec70932e5c5f4ea7ee421b3dc12728fb0edc8
Ariana Cabana <acabana.0904@gmail.com>
2017-06-03 23:38:46 -0500

    comprobando funcionalidad con jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,12 @@
-$( () => { //function() {}
-  $(".popup-link").on('click',(e) => {
-    e.preventDefault();
-    $('.brand-popup').show();
-    //$('.brand-popup').removeClass('hide');
-    //$('.brand-popup').addClass('show');
+// $( () => { //function() {}
+//   $(".popup-link").on('click',(e) => {
+//     e.preventDefault();
+//     $('.brand-popup').show();
+//     //$('.brand-popup').removeClass('hide');
+//     //$('.brand-popup').addClass('show');
+//
+//   });
 
-  });
+$(".btn").click(function(){
+  console.log("ya funcionaaaaaaaaaaa");
+})
