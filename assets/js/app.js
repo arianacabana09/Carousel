f06cf68598d846362d3ee64c9602dc1b29937ceb
Ariana Cabana <acabana.0904@gmail.com>
2017-06-04 23:08:52 -0500

    slider carousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,20 @@
-// $( () => { //function() {}
-//   $(".popup-link").on('click',(e) => {
-//     e.preventDefault();
-//     $('.brand-popup').show();
-//     //$('.brand-popup').removeClass('hide');
-//     //$('.brand-popup').addClass('show');
-//
-//   });
+// $(".btn").click(function(){
+//   console.log("ya funcionaaaaaaaaaaa");
+// })
 
-$(".btn").click(function(){
-  console.log("ya funcionaaaaaaaaaaa");
-})
+$( () => {
+  var time = 5000;
+  var duration = 1000;
+  var $show = $('.carousel');
+  var $slide = $show.children();
+
+  $slide.eq(0).fadeIn(duration);
+
+  setInterval(function(){
+    $slide = $show.children();
+    $slide.eq(0).fadeOut(duration, function(){
+      $(this).appendTo($show);
+    });
+    $slide.eq(0).fadeIn(duration);
+  }, time);
+});
